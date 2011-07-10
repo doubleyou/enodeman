589797a823287e71c6261123e717c16c87fc5c57
Dmitry Demeshchuk <demeshchuk@gmail.com>
2011-07-10 21:05:47 +0400

    Oops

--- src/enodeman_calendar.erl
+++ src/enodeman_calendar.erl
@@ -5,19 +5,19 @@
 
 %% TODO: use PropEr here for tests!!!
 
-days({Y1, all, all}, {Y2, all, all}) when Y1 > Y2 ->
+days_between({Y1, all, all}, {Y2, all, all}) when Y1 > Y2 ->
     [];
-days({Y1, all, all}, {Y2, all, all}) ->
+days_between({Y1, all, all}, {Y2, all, all}) ->
     lists:foldl(
         fun(Y, Acc) ->
-            Acc ++ days({Y, 1, all}, {Y, 12, all})
+            Acc ++ days_between({Y, 1, all}, {Y, 12, all})
         end,
         [],
         lists:seq(Y1, Y2)
     );
-days({Y, M1, all}, {Y, M2, all}) when M1 > M2 ->
+days_between({Y, M1, all}, {Y, M2, all}) when M1 > M2 ->
     [];
-days({Y, M1, all}, {Y, M2, all}) ->
+days_between({Y, M1, all}, {Y, M2, all}) ->
     lists:foldl(
         fun (M, Acc) ->
             Acc ++ [{Y, M, D}
@@ -26,15 +26,15 @@ days({Y, M1, all}, {Y, M2, all}) ->
         [],
         lists:seq(M1, M2)
     );
-days({Y, M, D1}, {Y, M, last}) ->
-    days({Y, M, D1}, {Y, M, calendar:last_day_of_the_month(Y, M)});
-days({Y, M, D1}, {Y, M, D2}) ->
+days_between({Y, M, D1}, {Y, M, last}) ->
+    days_between({Y, M, D1}, {Y, M, calendar:last_day_of_the_month(Y, M)});
+days_between({Y, M, D1}, {Y, M, D2}) ->
     [{Y, M, D} || D <- lists:seq(D1, D2)];
-days({Y, M1, D1}, {Y, M2, D2}) ->
-    days({Y, M1, D1}, {Y, M1, last}) ++
-    days({Y, M1 + 1, all}, {Y, M2 - 1, all}) ++
-    days({Y, M2, 1}, {Y, M2, D2});
-days({Y1, M1, D1}, {Y2, M2, D2}) ->
-    days({Y1, M1, D1}, {Y1, 12, last}) ++
-    days({Y1 + 1, all, all}, {Y2 - 1, all, all}) ++
-    days({Y2, 1, 1}, {Y2, M2, D2}).
+days_between({Y, M1, D1}, {Y, M2, D2}) ->
+    days_between({Y, M1, D1}, {Y, M1, last}) ++
+    days_between({Y, M1 + 1, all}, {Y, M2 - 1, all}) ++
+    days_between({Y, M2, 1}, {Y, M2, D2});
+days_between({Y1, M1, D1}, {Y2, M2, D2}) ->
+    days_between({Y1, M1, D1}, {Y1, 12, last}) ++
+    days_between({Y1 + 1, all, all}, {Y2 - 1, all, all}) ++
+    days_between({Y2, 1, 1}, {Y2, M2, D2}).
